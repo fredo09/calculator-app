93540829e2db7f5a667845b64e366da9e7ab7b7a
fredo09 <fred_15@outlook.com>
2025-02-06 00:29:29 -0600

    add dependencies and refactor component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import { Slot } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 
 import { globalStyles } from "@/styles/global.styles";
+
+import * as NavigationBar from 'expo-navigation-bar';
+
+// * is Android o Ios
+const isAndroid  = Platform.OS === 'android';
+
+if (isAndroid) {
+  NavigationBar.setBackgroundColorAsync('black');
+}
 
 const RootLayout = () => {
   //* Recuperamos el tipo de fuente para la app
--- components/CalculatorButton/index.tsx
+++ components/CalculatorButton/index.tsx
@@ -4,24 +4,35 @@ import { Pressable, Text } from 'react-native';
 import { globalStyles } from '@/styles/global.styles';
 import { Colors } from '@/constants/Colors';
 
-interface Props {
-  label: string;
-  color?: string;
-  dobleSize?: boolean;
-  blackText?: boolean;
+import * as Haptics from 'expo-haptics';
 
-  // Methods
-  onPress: () => void;
+interface Props {
+    label: string;
+    color?: string;
+    dobleSize?: boolean;
+    blackText?: boolean;
+
+    // Methods
+    onPress: () => void;
 }
 
-export const CalculatorButton = ({ label, onPress, blackText = false, dobleSize = false, color = Colors.darkGray }: Props) => {
+export const CalculatorButton = ({ 
+        label, 
+        onPress, 
+        blackText = false, 
+        dobleSize = false, 
+        color = Colors.darkGray }: Props
+    ) => {
     return (
         <Pressable style={({ pressed }) => ({
             ...globalStyles.button,
             backgroundColor: color,
             opacity: pressed ? 0.5 : 1,
             width: dobleSize ? 180 : 80,
-        })} onPress={onPress}>
+        })} onPress={() => {
+            onPress();
+            Haptics.selectionAsync()
+        }}>
             <Text 
                 style={{
                     ...globalStyles.buttonText,
